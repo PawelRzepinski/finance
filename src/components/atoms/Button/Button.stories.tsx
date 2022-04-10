b628a38fd8926372cba709c5b223eb20bbd6bf1c
Paweł <pawel.rzepinski@gmail.com>
2022-04-10 14:59:46 +0200

    Update button

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -4,7 +4,23 @@ import { Button } from './Button'
 
 export default {
   title: 'Atoms/Buttons/Button',
-  component: Button
+  component: Button,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large']
+      },
+      defaultValue: 'medium'
+    },
+    view: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary', 'square']
+      },
+      defaultValue: 'primary'
+    }
+  }
 } as ComponentMeta<typeof Button>
 
 export const Primary: ComponentStory<typeof Button> = (arg) => <Button {...arg}>Primary</Button>
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { forwardRef, ReactNode, useRef } from 'react'
 import styled, { css } from 'styled-components'
 
 interface ButtonProps {
@@ -8,23 +8,52 @@ interface ButtonProps {
   onClick: () => void
   size?: 'small' | 'medium' | 'large'
   view?: 'primary' | 'secondary' | 'square'
+  wide?: boolean
 }
 
-export const Button = ({ children, className, disabled, onClick, ...props }: ButtonProps) => {
-  return (
-      <ButtonWrapper className={className} disabled={disabled} onClick={onClick} {...props}>{children}</ButtonWrapper>
-  )
-}
-
-const ButtonWrapper = styled.button<Pick<ButtonProps, 'view'>>`
+// eslint-disable-next-line no-undef,react/display-name
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className, disabled, onClick, wide, ...props }) => {
+    // eslint-disable-next-line no-undef
+    const refContainer = useRef<HTMLButtonElement>(null)
+    return (
+      <ButtonWrapper className={className} disabled={disabled} onClick={onClick} wide={wide} ref={refContainer} {...props}>{children}</ButtonWrapper>
+    )
+  }
+)
+const ButtonWrapper = styled.button<Pick<ButtonProps, 'view' | 'size'>>`
   overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: red;
+  background-color: ${({ theme, view }) => view === 'primary' ? theme.colors.primary[50] : theme.colors.white[100]};
   border: none;
   border-radius: ${({ theme }) => theme.borderRadius.s};
   cursor: pointer;
+  
+  height: ${({ size }) => {
+    switch (size) {
+      case 'small':
+        return '32px'
+      case 'large':
+        return '50px'
+      case 'medium':
+      default:
+        return '40px'
+    }
+  }};
+
+  padding: ${({ size }) => {
+    switch (size) {
+      case 'small':
+        return '0 16px'
+      case 'large':
+        return '0 20px'
+      case 'medium':
+      default:
+        return '0 24px'
+    }
+  }};
   
   ${({ view }) => view === 'square' && css`
     width: 32px;
